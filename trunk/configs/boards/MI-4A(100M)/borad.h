016b24bac19957858bdfe4d96d3a50b847173118
shenmeiqian <59081526+shenmeiqian@users.noreply.github.com>
2020-06-06 22:34:07 +0800

    Update borad.h

--- trunk/configs/boards/MI-4A(100M)/borad.h
+++ trunk/configs/boards/MI-4A(100M)/borad.h
@@ -1,7 +1,7 @@
-/* Xiaomi Mi 4A 100m*/
+/* Xiaomi Mi 4A (100M)*/
 
-#define BOARD_PID		"MI-4A-100M"
-#define BOARD_NAME		"MI-4A-100M"
+#define BOARD_PID		"MI-4A(100M)"
+#define BOARD_NAME		"MI-4A(100M)"
 #define BOARD_DESC		"Xiaomi Mi 4A 100M Wireless Router"
 #define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
 #define BOARD_VENDOR_URL	"http://www.mi.com/"
@@ -15,9 +15,9 @@
 #define BOARD_GPIO_LED_POWER	24
 #undef  BOARD_GPIO_LED_LAN
 #undef  BOARD_GPIO_LED_WAN
-#define BOARD_HAS_5G_11AC	0
-#define BOARD_NUM_ANT_5G_TX	0
-#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
 #define BOARD_NUM_ANT_2G_TX	2
 #define BOARD_NUM_ANT_2G_RX	2
 #define BOARD_NUM_ETH_LEDS	1
