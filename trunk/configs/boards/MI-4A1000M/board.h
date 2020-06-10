e6e4d4859919e2c73f682f4bd66730e475a37a17
shenmeiqian <59081526+shenmeiqian@users.noreply.github.com>
2020-06-10 23:27:03 +0800

    Update board.h

--- trunk/configs/boards/MI-4A1000M/board.h
+++ trunk/configs/boards/MI-4A1000M/board.h
@@ -1,27 +1,27 @@
 /* Xiaomi Mi 4A 100M*/
 
-#define BOARD_PID		          "MI-4A100M"
-#define BOARD_NAME		        "MI-4A100M"
-#define BOARD_DESC		        "Xiaomi Mi 4A 100M Wireless Router"
-#define BOARD_VENDOR_NAME	    "Beijing Xiaomi Technology Co., Ltd."
-#define BOARD_VENDOR_URL	    "http://www.mi.com/"
-#define BOARD_MODEL_URL		    "http://www.mi.com/miwifilite"
-#define BOARD_BOOT_TIME		    25
-#define BOARD_FLASH_TIME	    120
-#define BOARD_GPIO_BTN_RESET	18
+#define BOARD_PID   "MI-4A1000M"
+#define BOARD_NAME    "MI-4A1000M"
+#define BOARD_DESC		"Xiaomi Mi 4A 1000M Wireless Router"
+#define BOARD_VENDOR_NAME "Beijing Xiaomi Technology Co., Ltd."
+#define BOARD_VENDOR_URL  "http://www.mi.com/"
+#define BOARD_MODEL_URL   "http://www.mi.com/miwifilite"
+#define BOARD_BOOT_TIME   25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET  18
 #define BOARD_GPIO_BTN_WPS
 #define BOARD_GPIO_LED_ALL
 #define BOARD_GPIO_LED_WIFI   11
-#define BOARD_GPIO_LED_POWER	24
+#define BOARD_GPIO_LED_POWER  24
 #undef BOARD_GPIO_LED_LAN
 #undef BOARD_GPIO_LED_WAN
 #undef BOARD_GPIO_LED_ROUTER
-#define BOARD_HAS_5G_11AC	    1
-#define BOARD_NUM_ANT_5G_TX	  2
-#define BOARD_NUM_ANT_5G_RX	  2
-#define BOARD_NUM_ANT_2G_TX	  2
-#define BOARD_NUM_ANT_2G_RX	  2
-#define BOARD_NUM_ETH_LEDS	  1
-#define BOARD_NUM_ETH_EPHY	  3
-#define BOARD_HAS_EPHY_L1000	0
-#define BOARD_HAS_EPHY_W1000	0
+#define BOARD_HAS_5G_11AC 1
+#define BOARD_NUM_ANT_5G_TX 2
+#define BOARD_NUM_ANT_5G_RX 2
+#define BOARD_NUM_ANT_2G_TX 2
+#define BOARD_NUM_ANT_2G_RX 2
+#define BOARD_NUM_ETH_LEDS  1
+#define BOARD_NUM_ETH_EPHY  3
+#define BOARD_HAS_EPHY_L1000  1
+#define BOARD_HAS_EPHY_W1000	1
