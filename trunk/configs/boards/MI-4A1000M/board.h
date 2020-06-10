70a6d65ce4ea7238a1cffde843ed06f52e55935e
shenmeiqian <59081526+shenmeiqian@users.noreply.github.com>
2020-06-11 00:08:48 +0800

    Update board.h

--- trunk/configs/boards/MI-4A1000M/board.h
+++ trunk/configs/boards/MI-4A1000M/board.h
@@ -5,7 +5,7 @@
 #define BOARD_DESC		"Xiaomi Mi 4A1000M Wireless Router"
 #define BOARD_VENDOR_NAME	"Beijing Xiaomi Technology Co., Ltd."
 #define BOARD_VENDOR_URL	"http://www.mi.com/"
-#define BOARD_MODEL_URL		"http://www.mi.com/miwifir3g"
+#define BOARD_MODEL_URL		"http://www.mi.com/"
 #define BOARD_BOOT_TIME		25
 #define BOARD_FLASH_TIME	120
 #define BOARD_GPIO_BTN_RESET	18
@@ -15,10 +15,6 @@
 #define BOARD_GPIO_LED_POWER	24
 #define BOARD_GPIO_LED_LAN	10
 #undef  BOARD_GPIO_LED_WAN
-#undef  BOARD_GPIO_LED_USB
-#undef  BOARD_GPIO_LED_ROUTER
-#undef BOARD_GPIO_PWR_USB_ON		/* 1: 5V Power ON, 0: 5V Power OFF */
-#undef BOARD_GPIO_PWR_USB		/* USB2.0 5V Power */
 #define BOARD_HAS_5G_11AC	1
 #define BOARD_NUM_ANT_5G_TX	2
 #define BOARD_NUM_ANT_5G_RX	2
@@ -28,4 +24,3 @@
 #define BOARD_NUM_ETH_EPHY	3
 #define BOARD_HAS_EPHY_L1000	1
 #define BOARD_HAS_EPHY_W1000	1
-#undef BOARD_NUM_UPHY_USB3
