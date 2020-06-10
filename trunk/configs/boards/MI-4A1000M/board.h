780f9a1e7ef8d4bcad29402188113c0149fba52b
shenmeiqian <59081526+shenmeiqian@users.noreply.github.com>
2020-06-11 01:14:12 +0800

    Update board.h

--- trunk/configs/boards/MI-4A1000M/board.h
+++ trunk/configs/boards/MI-4A1000M/board.h
@@ -8,7 +8,7 @@
 #define BOARD_MODEL_URL		"http://www.mi.com/miwifilite"
 #define BOARD_BOOT_TIME		25
 #define BOARD_FLASH_TIME	120
-#define BOARD_GPIO_BTN_RESET	38
+#define BOARD_GPIO_BTN_RESET	18
 #undef  BOARD_GPIO_BTN_WPS
 #undef  BOARD_GPIO_LED_ALL
 #define BOARD_GPIO_LED_WIFI	11
